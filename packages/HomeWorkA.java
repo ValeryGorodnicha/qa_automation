421fac44e652f86573b10148bdd92fdafe797ae4
Valery <vgorodnicha@gmail.com>
2023-08-03 21:25:52 +0300

    Add HomeWorkA.java file

---
+++ packages/HomeWorkA.java
@@ -0,0 +1,18 @@
+package valerypackage;
+
+public class HomeWorkA {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 10;
+
+        int c = 3;
+        int d = 5;
+        System.out.println(Math.pow(a, b));
+        System.out.println(Math.pow(b, a));
+        System.out.println();
+        System.out.println(Math.pow(c, d));
+        System.out.println(Math.pow(d, c));
+
+    }
+}
+
